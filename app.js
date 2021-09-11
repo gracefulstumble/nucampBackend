15fee5038741780388c40cfefc4eef2acd75d338
Emily Gonzales <emilyg89503@gmail.com>
2021-09-11 07:29:48 -0700

    express sessions part 1

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const FileStore = require('session-file-store')(session); //require is returning session-file-store function as the return value and then immediately calling the session param list
+
 const url = 'mongodb://localhost:27017/nucampsite';
 const connect = mongoose.connect(url, {
     useCreateIndex: true,
@@ -20,32 +23,39 @@ const campsiteRouter = require('./routes/campsiteRouter');
 const promotionRouter = require('./routes/promotionRouter');
 const partnerRouter = require('./routes/partnerRouter');
 var app = express();
-
+// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser('12345-67890-09876-54321')); 
+//app.use(cookieParser('12345-67890-09876-54321')); //using signed cookies, so we provide the cookie parser with a secret key as an argument, can be anything
 
+app.use(session({
+  name: 'session-id',
+  secret: '12345-67890-09876-54321',
+  saveUninitialized: false,
+  resave: false,
+  store: new FileStore()  //save to server hard disk instead of running in application memory
+}));
 
-
+//authentication
 function auth(req, res, next) {
-  if (!req.signedCookies.user) {
-    const authHeader = req.headers.authorization; 
-    if (!authHeader) {  
+  console.log(req.session);
+  if (!req.session.user) {
+    const authHeader = req.headers.authorization; //get authorization header
+    if (!authHeader) {  //no authentication information returned, user has not entered credentials
         const err = new Error('You are not authenticated!');
-        res.setHeader('WWW-Authenticate', 'Basic'); 
+        res.setHeader('WWW-Authenticate', 'Basic'); //lets client know that server is requesting authentication and that the auth method being requested is basic
         err.status = 401;
         return next(err);
     }
-
-    const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':'); 
+    const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':'); //use Node global, Buffer, to parse out the username and password into a new "auth" array
     const user = auth[0];
     const pass = auth[1];
     if (user === 'admin' && pass === 'password') {
-      res.cookie('user', 'admin', {signed: true});  
-      return next(); 
+      req.session.user = 'admin';
+      return next(); // authorized
         } else {
             const err = new Error('You are not authenticated!');
             res.setHeader('WWW-Authenticate', 'Basic');
@@ -53,8 +63,8 @@ function auth(req, res, next) {
             return next(err);
         }
       } else {
-        if (req.signedCookies.user === 'admin') {
-            return next();  
+        if (req.session.user === 'admin') {
+            return next();  //authorized
         } else {
         const err = new Error('You are not authenticated!');
         err.status = 401;
@@ -62,7 +72,6 @@ function auth(req, res, next) {
     }
   }
 }
-
 app.use(auth);
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
@@ -70,16 +79,16 @@ app.use('/users', usersRouter);
 app.use('/campsites', campsiteRouter);
 app.use('/promotions', promotionRouter);
 app.use('/partners', partnerRouter);
-
+// catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
-
+// error handler
 app.use(function(err, req, res, next) {
- 
+  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-  
+  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
